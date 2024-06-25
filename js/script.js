82f5508fac27fd78662af5b31cf5d8ccaa647229
Patryk Majchrzak <pmaj12345@gmail.com>
2024-06-25 23:43:16 +0200

    closing code responsible for changing photo in changePhoto function

--- js/script.js
+++ js/script.js
@@ -3,16 +3,11 @@
 
     welcome ();
     
-    const photoMain = document.querySelector(".js-photoMain");
-    const photoAngel = document.querySelector(".js-photoAngel");
-    const photoFlowers = document.querySelector(".js-photoFlowers");
-    const imageButton = document.querySelector(".js-imageButton");
-    const backgroundButton = document.querySelector(".js-backgroundButton");
-    const background = document.querySelector(".js-document");
-    const shadeName = document.querySelector(".js-shadeName");
-    const hidden = "head__photo--hidden";
+    const changePhoto = () => {
+        const photoMain = document.querySelector(".js-photoMain");
+        const photoAngel = document.querySelector(".js-photoAngel");
+        const photoFlowers = document.querySelector(".js-photoFlowers");
 
-    imageButton.addEventListener("click", () => {
         if (photoAngel.classList.contains(hidden) && photoFlowers.classList.contains(hidden)) {
             photoMain.classList.toggle(hidden);
             photoAngel.classList.toggle(hidden);
@@ -25,12 +20,18 @@
             photoFlowers.classList.toggle(hidden);
             photoMain.classList.toggle(hidden);
         };
-    });
+    }
+
+    const imageButton = document.querySelector(".js-imageButton");
+    const backgroundButton = document.querySelector(".js-backgroundButton");
+    const background = document.querySelector(".js-document");
+    const shadeName = document.querySelector(".js-shadeName");
+    const hidden = "head__photo--hidden";
+
+    imageButton.addEventListener("click", changePhoto());
 
     backgroundButton.addEventListener("click", () => {
         background.classList.toggle("document--lightColor");
         shadeName.innerText = background.classList.contains("document--lightColor") ? "ciemne" : "jasne"
     });
-
-    welcome()
 };
